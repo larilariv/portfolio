d462741855be70b4357bb524972f9c9db51b4d92
Larissa Vieira <lv23@nyu.edu>
2022-08-07 23:53:58 -0400

    All componenets imported into App.js and routes complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import { About, Footer, Home, Navbar, Projects } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/about" element={<About />} />
+        </Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
