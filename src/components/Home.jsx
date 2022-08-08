bb17db0e2e30e4e3b69f7009357ffd8301f71117
Larissa Vieira <lv23@nyu.edu>
2022-08-07 23:50:36 -0400

    Image file name updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="min-h-screen max-w-screen-xl mx-auto px-6 lg:px-8">
       <img
-        src="images/completeLineArt2.png"
+        src="images/completeLineArt.png"
         className="max-w-full mt-20"
         alt="Line drawing of woman at computer"
       />
