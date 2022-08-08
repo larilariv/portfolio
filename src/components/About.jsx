e2b1bd8054b0fbcf08dc1c02306b0e1dd62b4348
Larissa Vieira <lv23@nyu.edu>
2022-08-07 23:52:10 -0400

    About component created, still needs work

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="min-h-screen max-w-screen-xl mx-auto px-6 lg:px-8">
+      <a
+        href="https://drive.google.com/file/d/1bqQkvz2KT7nsQtsT6mCRfSBb0sT-Yj3J/view?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        View Resume
+      </a>
+      <iframe
+        src="https://docs.google.com/document/d/e/2PACX-1vTRfszzsO2uY0lT2H-0rfJBIGEQHzyZPhNHQ2fyaNIdTCpjjly-wfX-7mGaReYsSgF5RDHiZPa1rfoX/pub?embedded=true"
+        className="w-full aspect-video md:aspect-square"
+      ></iframe>
+    </div>
+  );
+};
+
+export default About;
