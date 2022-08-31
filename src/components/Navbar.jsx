b9ec4b203fc1805c8a83c6d9cc694eb3be71b19d
Larissa Vieira <lv23@nyu.edu>
2022-08-30 20:54:38 -0400

    Vertical navbar added for smaller screens and Contact dropdown shifted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
               <span>Larissa Vieira</span>
             </a>
           </div>
-
           {/* Navbar Collapse Button */}
           <button
             className="inline-flex items-center p-2 ml-3 text-sm text-sage-50 bg-sage-600 rounded-lg md:hidden hover:bg-sage-500 hover:text-white"
@@ -37,16 +36,12 @@ const Navbar = () => {
             </svg>
           </button>
 
-          <nav className={"md:flex" + (navbarOpen ? " flex" : " hidden")}>
+          <nav className="hidden md:flex">
             <h2 className="sr-only" id="header-navigation">
               Header navigation
             </h2>
 
-            <ul
-              className={
-                "flex text-md" + (navbarOpen ? " flex-col" : "flex-row gap-6")
-              }
-            >
+            <ul className="flex text-md flex-row gap-6">
               <li className="nav-item">
                 <a
                   className="block text-sage-50 transform transition duration-500 hover:scale-110 hover:text-white"
@@ -87,7 +82,7 @@ const Navbar = () => {
                     <button type="button"></button>
 
                     <div
-                      className="absolute right-0 z-10 w-54 mt-4 origin-top-right bg-sage-600 rounded-md shadow-lg"
+                      className="absolute -right-24 z-10 w-44 mt-4 bg-sage-600 rounded-lg shadow-lg"
                       role="menu"
                     >
                       <div
@@ -157,6 +152,105 @@ const Navbar = () => {
           </nav>
         </div>
       </div>
+
+      <nav
+        className={
+          "flex flex-col justify-between px-4 pt-0 py-6 md:hidden" +
+          (navbarOpen ? "flex" : " hidden")
+        }
+      >
+        <a href="/" className="block px-6 py-2 text-sage-50 hover:text-white">
+          Home
+        </a>
+
+        <a
+          href="/about"
+          className="block px-6 py-2 text-sage-50 hover:text-white"
+        >
+          About
+        </a>
+
+        <a
+          href="/projects"
+          className="block px-6 py-2 text-sage-50 hover:text-white"
+        >
+          Projects
+        </a>
+
+        <details className="group">
+          <summary className="flex items-center px-6 py-2 text-sage-50 hover:text-white">
+            Contact
+            <span className="ml-auto transition duration-300 shrink-0 group-open:-rotate-180">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <nav className="mt-1.5 ml-8 flex flex-col">
+            <a
+              href="https://github.com/larilariv"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex px-2 py-2 text-sm text-sage-50 hover:text-white"
+              role="menuitem"
+            >
+              GitHub
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/larilari-vieira"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex px-2 py-2 text-sm text-sage-50 hover:text-white"
+              role="menuitem"
+            >
+              LinkedIn
+            </a>
+
+            <button
+              className="inline-flex px-2 py-2 text-sm text-sage-50 hover:text-white"
+              href="/#"
+              role="menuitem"
+              onClick={() => navigator.clipboard.writeText("lv23@nyu.edu")}
+            >
+              lv23@nyu.edu
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="ml-1.5 w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+            </button>
+          </nav>
+        </details>
+
+        <a
+          href="https://drive.google.com/file/d/1bqQkvz2KT7nsQtsT6mCRfSBb0sT-Yj3J/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          className="px-6 py-3 bg-sage-600 rounded-lg text-sage-50 hover:text-white hover:bg-sage-500"
+        >
+          <div className="relative inline-flex block">Resume</div>
+        </a>
+      </nav>
     </div>
   );
 };
